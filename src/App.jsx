6356b44e5c011bf2a26e2bf18cd10d3324942635
tigist609 <tegie88@yahoo.com>
2025-07-01 17:28:39 -0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./app.css";
+import "./App.css";
 import FoodItem from "./commonResource/FoodItem/FoodItem.jsx";
 import menu from "./commonResource/data.js";
 import "./commonResource/style.css";
